fe87ac2ca10e23c47988a557fc43ad492a68eb01
mhd-rawashdah <mhd.rawashdah@gmail.com>
2018-11-05 19:49:09 +0200

    react component adding comments

--- app.js
+++ app.js
@@ -25,26 +25,26 @@
 // 	};
 
 
-
+ // GroceryListItem component to return <li> element of the list with some of event 
 class GroceryListItem extends React.Component {
-
+ 
 	constructor (props) {
 		super(props);
-
+    // defined state with object has hover property
 		this.state = {
 			hover: false
 		}
 	}
-
+  // method to setState if the mouse hover on the list
 	onMouseEnter() {
 		this.setState({	hover : true});
 	}
-
+  //method to setState if the mouse hover on the list
 	onMouseLeave() {
 		this.setState({	hover : false});
 	}
 
-   
+  // render method to return the element
 	render () {
 		var style = {
 			fontWeight: this.state.hover ? 'bold' : 'normal'
@@ -59,24 +59,26 @@ class GroceryListItem extends React.Component {
 	}
 }
 
-
+// Grocery component to show all item in the list by using GroceryListItem component
 class GroceryList extends React.Component {
 	constructor(props) {
 		super(props);
 	}
-	 render () {
-     return (  
-	   		<ul>
-				  {this.props.items.map( item =>	
-					  <GroceryListItem items={item}/>
-				  )}
-	      </ul>
-	   )
+	// render unorder list and using GroceryListItem component to do that.
+	render () {
+    return (
+    //use the map to go over the array  that pass when call this component in the property items    
+	   	<ul>   	
+				{this.props.items.map( item =>	
+					<GroceryListItem items={item}/>
+				)}
+	    </ul>
+	  )
 	}
 
 }
 
-
+// render the Grocery list on user interface and put the component int the index html
 ReactDOM.render(<GroceryList items= {["Banana", "Apple", "tomato", "lemon"]} />, document.getElementById('app'));
 
 
